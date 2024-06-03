a618844503a5af2b41747abb58949cb138d15140
Bice Marzagora <59375165+BiceMar@users.noreply.github.com>
2024-06-03 14:53:37 +0200

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=matrix_multiplication
+#SBATCH --output=result.txt
+#SBATCH --error=error.txt
+#SBATCH --time=01:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+
+# Load Singularity module
+module load singularity
+
+# Run the containerized application
+#singularity exec /path/to/my_container.sif /path/to/matrix_multiplication_program
+echo "Testtttt!"
